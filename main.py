4ade872f780a1f1226aad9aa23cbe93ac2bd27e1
Swelio <24651896+Swelio@users.noreply.github.com>
2018-09-01 11:23:25 +0200

    Delete new options using (but still present)

--- main.py
+++ main.py
@@ -11,15 +11,11 @@ from classifier_class import Classifier
 if __name__ == '__main__':
     resources = '.'
     test = '.'
-    # optional dictionary
-    dictionary_path = '.'
 
     detector = Classifier(resources,
                           weights_file='CNN_authors',
                           classifier_save_path='CNN_authors_detector',
                           fit_epochs=10,
                           data_size_max=10,
-                          sequence_length=200,
-                          reuse_datas=False,
-                          dict_path=dictionary_path)
+                          sequence_length=200)
     detector.display_prediction(test)
