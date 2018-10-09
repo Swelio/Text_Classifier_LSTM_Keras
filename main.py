eca0804931344e06733247f7d8cbbe18ad169a69
Swelio <24651896+Swelio@users.noreply.github.com>
2018-10-09 10:12:48 +0200

    test

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from classifier_class import Classifier
 
 # ---- Script ----
 if __name__ == '__main__':
-    resources = '.'
-    test = '.'
-    test2 = '.'
+    resources = 'C:'  # ignore
+    test = 'C:/Us'  # ignore
+    test2 = 'C:/'  # ignore
 
     detector = Classifier(resources,
                           weights_file='languages',
